1d88e3fe459144f2c5ef4edab196ff0c6e49a61d
eqvyoo <ys07106@gmail.com>
2023-03-26 01:06:58 +0900

    upload day5 files

---
+++ study_5/src/ScientificCalculator.java
@@ -0,0 +1,5 @@
+public class ScientificCalculator extends Calculator {
+    public int square(int a){
+        return super.multiply(a,a);
+    }
+}
---
+++ study_5_2/src/AngryCat.java
@@ -0,0 +1,5 @@
+public class AngryCat extends Cat {
+    public void soundAngry() {
+        System.out.println("크르르르르르냐아아아앙");
+    }
+}
